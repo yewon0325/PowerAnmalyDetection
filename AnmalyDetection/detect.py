60e39428ca80741b3e43961446e4e5f794049628
dohyeoni <2dh18@naver.com>
2024-12-11 00:10:39 +0900

    Update detect.py

--- AnmalyDetection/detect.py
+++ AnmalyDetection/detect.py
@@ -29,9 +29,9 @@ else:
 
 # Firebase Admin SDK 초기화
 if not firebase_admin._apps:
-    cred = credentials.Certificate('./anomaly-detection-9939e-firebase-adminsdk-l9emo-0138e39dc1.json')
+    cred = credentials.Certificate()
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://anomaly-detection-9939e-default-rtdb.asia-southeast1.firebasedatabase.app/'
+        'databaseURL': 
     })
 
 # 설정 상수
